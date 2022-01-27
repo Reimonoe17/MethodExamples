0329006a5596bdf21c88e169d8b9341faaaf0857
Jamison Burton <jamison@burtontalk.com>
2022-01-27 08:08:59 -0700

    First Upload

---
+++ MethodExamples/MethodExamples.vb
@@ -0,0 +1,25 @@
+﻿Option Explicit On
+Option Strict On
+Option Compare Text
+
+Module MethodExamples
+
+    Sub Main()
+
+        AddTwoNumbers() 'Calls subroutine
+        SayAMessage()
+
+        Console.Read()
+    End Sub
+
+    Sub AddTwoNumbers()
+        Dim result As Integer
+        result = 2 + 2 'result only exists in the subroutine, outside of it nothing can be done with it
+    End Sub
+
+    Sub SayAMessage()
+        Console.WriteLine("Howdy!")
+    End Sub
+
+
+End Module
